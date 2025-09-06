bcf5fe1801c6739f01aa4bd164adee89f16ee427
Shravan Modukuri <shravanram199715@gmail.com>
2025-09-06 22:56:36 +0530

    Js class added

--- public/Test.js
+++ public/Test.js
@@ -7,3 +7,17 @@ const person = {
 };
 
 person.greet(); // Hi, I'm John
+
+
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+
+  speak() {
+    console.log(this.name + " makes a sound");
+  }
+}
+
+const dog = new Animal("Dog");
+dog.speak(); // Dog makes a sound
